7243ab3a23a898d3ac2ce55abf520f143f842ae8
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-05-30 16:19:52 -0400

    ADD: Tip 3: Chain Opertaor

---
+++ Beginner/ChainOperator.py
@@ -0,0 +1,23 @@
+# The 3rd Tips&Topics for Beginner 
+
+# Chain Operator...
+
+## Traditional way to compare variable (Larger Than and Smaller Than)...
+
+var = 10
+var2 = 5
+var3 = 20
+print(var < var3 and var > var2) # Output : True , coz 10 is smaller than 20 and larger than 20
+
+  ## Or ...
+
+## Using the chain operator : compare like a mathematician
+# Verify if var is between two variables or not ..
+
+var = 10
+var2 = 5
+var3 = 20
+print(var2 < var < var3) # Output : True, 
+
+     
+
