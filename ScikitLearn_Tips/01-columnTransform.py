14a9e0dacd53a5f5c341884a8169350dfcceac5f
th-rpy <thamer.saraei@usherbrooke.ca>
2022-08-25 13:15:55 -0400

    Create ColumnTransformer object

--- ScikitLearn_Tips/01-columnTransform.py
+++ ScikitLearn_Tips/01-columnTransform.py
@@ -8,7 +8,38 @@
 # Example :
 
 # Import the necessary libraries
+import pandas as pd  # For dataframes
+from sklearn.compose import make_column_transformer  # For ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler  # For preprocessing
 
 
-# select from DataFrame columns by name
-# passthrough or drop unspecified columns
+# Read the data from the file
+df = pd.read_csv("data/data.csv")
+df.info()  # To see the dataframe info
+print(df.columns)  # To see the dataframe columns names :
+
+""" Output: Index(['ml_experience', 'major', 'class_attendance',
+       'university_years', 'lab1', 'lab2', 'lab3', 'lab4', 'quiz1', 'quiz2'],
+      dtype='object')"""
+
+X = df.drop(columns=["quiz2"])  # Drop the quiz2 column from the dataframe
+y = df["quiz2"]  # We'll use the quiz2 column as the target
+
+# Create a ColumnTransformer object
+numeric_cols = ["university_years", "lab1", "lab3", "lab4", "quiz1"]  # apply scaling
+categorical_cols = ["major"]  # apply one-hot encoding
+passthrough_cols = ["ml_experience"]  # do not apply any transformation
+drop_cols = [
+    "lab2",
+    "class_attendance",
+]  # do not include these features in modeling
+
+ct = make_column_transformer(
+    (StandardScaler(), numeric_cols),  # scaling on numeric features
+    (OneHotEncoder(), categorical_cols),  # OHE on categorical features
+    ("passthrough", passthrough_cols),  # no transformations on the binary features
+    ("drop", drop_cols),  # drop the drop features
+)
+
+# Transform the data using the ColumnTransformer object. 
+X_transformed = ct.fit_transform(X) 
