9ba718253721500db407a0ccc0ab6e9e8de7d69f
th-rpy <thamer.saraei@usherbrooke.ca>
2022-08-25 12:56:05 -0400

    First Tip

---
+++ ScikitLearn_Tips/01-columnTransform.py
@@ -0,0 +1,14 @@
+# For this tip, we will need to use the following library:
+# - scikit-learn 0.20+
+# - pandas 0.20+
+#########################################################
+
+# Tip #1 : We'll use ColumnTransformer to apply different preprocessing to different columns.
+
+# Example :
+
+# Import the necessary libraries
+
+
+# select from DataFrame columns by name
+# passthrough or drop unspecified columns
